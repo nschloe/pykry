0b51c5e458a897f05acba3860f67e86d0792109a
Nico Schlömer <nico.schloemer@gmail.com>
2018-08-31 17:19:08 +0200

    stricter test

--- test/test_pykry.py
+++ test/test_pykry.py
@@ -9,12 +9,12 @@ def test_pykry():
     A = numpy.diag([1.0e-3] + list(range(2, 101)))
     b = numpy.ones(100)
 
-    sol = pykry.gmres(A, b)
-    print(sol)
-    exit(1)
+    out = pykry.gmres(A, b)
 
-    # # plot residuals
-    # from matplotlib import pyplot
-    # pyplot.semilogy(sol.resnorms)
-    # pyplot.show()
+    ref = 1004.1873724888546
+    assert abs(numpy.sum(numpy.abs(out.xk)) - ref) < 1.0e-14 * ref
+    ref = 1000000.6249262823
+    assert abs(numpy.dot(out.xk, out.xk) - ref) < 1.0e-14 * ref
+    ref = 999.999994971191
+    assert abs(numpy.max(numpy.abs(out.xk)) - ref) < 1.0e-14 * ref
     return
