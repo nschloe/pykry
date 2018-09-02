99b1538567671357ff84d70dbf77632015b65c5f
Nico Schlömer <nico.schloemer@gmail.com>
2018-09-02 07:47:45 +0200

    make black

--- test/test_deflation.py
+++ test/test_deflation.py
@@ -6,11 +6,14 @@ import pytest
 import pykry
 
 
-@pytest.mark.parametrize("method, ref", [
-    (pykry.cg, [1004.1873775173271, 1000.0003174918709, 1000.0]),
-    (pykry.minres, [1004.1873774950692, 1000.0003174918709, 1000.0]),
-    (pykry.gmres, [1004.1873774950692, 1000.0003174918709, 1000.0]),
-])
+@pytest.mark.parametrize(
+    "method, ref",
+    [
+        (pykry.cg, [1004.1873775173271, 1000.0003174918709, 1000.0]),
+        (pykry.minres, [1004.1873774950692, 1000.0003174918709, 1000.0]),
+        (pykry.gmres, [1004.1873774950692, 1000.0003174918709, 1000.0]),
+    ],
+)
 def test_gmres(method, ref):
     n = 100
     A = numpy.diag([1.0e-3] + list(range(2, n + 1)))
